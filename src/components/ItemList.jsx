3219df7a345d542afed21d91e9b134e532292794
sol di-falco <msoldifalco86@gmail.com>
2024-05-26 17:07:56 -0300

    .........

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,12 +4,6 @@ import logo from "./logo.jpg";
 const ItemList = ({ productos, titulo }) => {
   return (
     <div className="container">
-      <div className="banner">
-        <p>Unite al estilo monkey</p>
-        <img src={logo} alt="logo" />
-        <p>Remeras para todas las edades y de todos los talles</p>
-      </div>
-
       <div className="productos">
         {productos.map((prod) => (
           <Item producto={prod} key={prod.id} />
